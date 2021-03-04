0cf717c0f20fa06f0f23451624f64533f3c71327
Michal <michalmarchel62@gmail.com>
2021-03-04 08:29:14 +0100

    Project skeleton

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book() {
+  constructor (title, author, pages, read)
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  read = boolean;
+  }
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
